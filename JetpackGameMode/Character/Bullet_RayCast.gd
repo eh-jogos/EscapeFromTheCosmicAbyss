4367cc5296828338e2db349a410c630e0a1926f9
Daniel Queiroz Porto <danielvandalk@gmail.com>
2019-08-05 08:43:52 -0300

    Make laser get bigger as you improve it in the upgrade screen

--- JetpackGameMode/Character/Bullet_RayCast.gd
+++ JetpackGameMode/Character/Bullet_RayCast.gd
@@ -1,6 +1,10 @@
-extends RayCast2D
+extends Node2D
 
 # class member variables go here, for example:
+var raycast_top
+var raycast_bottom
+var raycasts
+
 var animator
 var idle_timer
 var idle_duration
@@ -10,24 +14,34 @@ signal laser_end
 
 func _ready():
 	#print("Bullet Ready")
+	raycast_top = get_node("TopRaycast")
+	raycast_bottom = get_node("BottomRaycast")
+	raycasts = [raycast_top, raycast_bottom]
+	
 	animator = self.get_node("AnimationPlayer")
 	animator.play("fire")
 	
 	idle_timer = self.get_node("IdleTimer")
 	
 	set_fixed_process(true)
-	pass
+
 
 func set_laser_duration(duration):
+	var new_height = Vector2(1,1)
+	new_height.y += duration
+	
 	idle_duration = duration - 0.3 - 0.5
+	self.set_scale(new_height)
+
 
 func _fixed_process(delta):
-	if self.is_colliding():
-		var collider = self.get_collider()
-		#print(collider.get_name())
-		if collider.is_in_group("pipes"):
-			#print("Bullet HIT")
-			collider.emit_signal("die")
+	for raycast in raycasts:
+		if raycast.is_colliding():
+			var collider = raycast.get_collider()
+			#print(collider.get_name())
+			if collider.is_in_group("pipes"):
+				#print("Bullet HIT")
+				collider.emit_signal("die")
 
 
 func start_idle_timer():
@@ -36,7 +50,7 @@ func start_idle_timer():
 	idle_timer.set_wait_time(idle_duration)
 	idle_timer.start()
 
-func _on_idle_timeout():
+func _on_IdleTimer_timeout():
 	animator.play("fadeout")
 	yield(animator, "finished")
 	emit_signal("laser_end")
