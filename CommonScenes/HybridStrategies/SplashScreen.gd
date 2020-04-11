d2a1c66fe2845c78217d038e032c624769a93b58
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-04-11 09:03:33 -0300

    Add background loading to the ScreenManager and use it on splashscreen

--- CommonScenes/HybridStrategies/SplashScreen.gd
+++ CommonScenes/HybridStrategies/SplashScreen.gd
@@ -3,5 +3,9 @@ extends ColorFrame
 var next_screen_path = "res://CommonScenes/MainMenu/MainMenuScreen.tscn"
 
 func change_scene():
-	ScreenManager.load_screen(next_screen_path)
+	ScreenManager.load_screen_invisible(next_screen_path)
+	pass
+
+func reveal_loading_screen():
+#	ScreenManager.reveal_invisible_loading_screen()
 	pass
--- CommonScenes/LoadingScreen/LoadingScreen.gd
+++ CommonScenes/LoadingScreen/LoadingScreen.gd
@@ -5,6 +5,7 @@ signal transition_ended
 signal loading_started
 
 signal scene_above_loaded(scene_node)
+signal scene_loaded(scene)
 
 # I learned this in this link 
 # http://docs.godotengine.org/en/stable/learning/features/misc/background_loading.html
@@ -14,6 +15,7 @@ var progress_bar
 
 var loader
 var animation_loaded = false
+var load_without_animation = false
 var time_max = 100 #msec
 
 var scene_above
@@ -29,6 +31,7 @@ func _ready():
 func reset():
 	progress_bar.set_value(0)
 	animation_loaded = false
+	load_without_animation = false
 
 func load_above(path, focus_path, origin_scene, path_is_node = false):
 	scenes_bellow.append(origin_scene)
@@ -52,13 +55,12 @@ func load_above(path, focus_path, origin_scene, path_is_node = false):
 
 func background_loading(path):
 	var loader = ResourceLoader.load_interactive(path)
+	var timer = get_node("Timer")
 	while not loader.poll() == ERR_FILE_EOF:
-		yield(get_tree(), "idle_frame")
-		yield(get_tree(), "idle_frame")
-		yield(get_tree(), "idle_frame")
-		yield(get_tree(), "idle_frame")
-		yield(get_tree(), "idle_frame")
-	return loader.get_resource()
+		timer.start()
+		yield(timer, "timeout")
+	
+	emit_signal("scene_loaded", loader.get_resource())
 
 
 func clear_above():
@@ -120,13 +122,26 @@ func load_screen(path):
 	
 	set_process(true)
 
+
+func load_screen_invisible(path):
+	load_without_animation = true
+	background_loading(path)
+	var loaded_scene = yield(self, "scene_loaded")
+	set_new_scene(loaded_scene)
+#	set_process(true)
+
+
+func reveal_invisible_loading_screen():
+	animation.play("fade_in")
+
+
 func _process(delta):
 	if loader == null:
 		set_process(false)
 		return
 	
 	var t = OS.get_ticks_msec()
-	while OS.get_ticks_msec() < t + time_max and animation_loaded:
+	if (animation_loaded or load_without_animation):
 		# poll your loader
 		var err = loader.poll()
 		
@@ -134,13 +149,11 @@ func _process(delta):
 			var resource = loader.get_resource()
 			loader = null
 			set_new_scene(resource)
-			break
 		elif err == OK:
 			update_progress()
 		else: # error during loading
 			show_error()
 			loader = null
-			break
 
 func update_progress():
 	var progress = (float(loader.get_stage()) / loader.get_stage_count())*100
@@ -150,10 +163,16 @@ func update_progress():
 
 func set_new_scene(scene_resource):
 	progress_bar.set_value(100)
+	if not animation_loaded:
+		animation.play("black_transition")
+		yield(animation, "finished")
 	
 	get_tree().change_scene_to(scene_resource)
 	
-	animation.play_backwards("fade_in")
+	if animation_loaded:
+		animation.play_backwards("fade_in")
+	else:
+		animation.play_backwards("black_transition")
 	yield(animation, "finished")
 	reset()
 
