669f9e1235eca53c946c67824e38c68ac7a23e4f
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-04-25 19:45:59 -0300

    WIP Updated Colors Menu with fully functional navigations and wip previews
    Just need to finish the preview animations and make the navigation change
    between them

--- CommonScenes/ColorsMenu/ColorsMenu.gd
+++ CommonScenes/ColorsMenu/ColorsMenu.gd
@@ -4,7 +4,9 @@ var animator
 
 func _ready():
 	animator = get_node("AnimationPlayer")
-
+	
+	var menu_block = get_node("Menu/MenuRoot/MenusBlock")
+	menu_block.current_menu = menu_block.current_menu
 
 
 func _on_Exit_pressed():
@@ -17,6 +19,37 @@ func _on_Reset_pressed():
 
 func close():
 	if get_tree().get_current_scene() == self:
+		var menu_root = get_node("Menu/MenuRoot")
+		menu_root.hide()
 		hide()
+		
 	else:
 		ScreenManager.black_transition_from_above()
+
+
+func _on_MenuWaves_activated():
+	pass # replace with function body
+
+
+func _on_MenuTentacles_activated():
+	pass # replace with function body
+
+
+func _on_MenuLaserEye_activated():
+	pass # replace with function body
+
+
+func _on_MenuBgBoss_activated():
+	pass # replace with function body
+
+
+func _on_MenuMidBoss_activated():
+	pass # replace with function body
+
+
+func _on_MenuFinalBoss_activated():
+	pass # replace with function body
+
+
+func _on_MenuFinalBoss1_activated():
+	pass # replace with function body
