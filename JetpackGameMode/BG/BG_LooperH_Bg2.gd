537c085cb2bab1f463c5225376d424db09c01878
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-04-28 07:52:03 -0300

    Fix parallax issues at game start

--- JetpackGameMode/BG/BG_LooperH_Bg2.gd
+++ JetpackGameMode/BG/BG_LooperH_Bg2.gd
@@ -1,18 +1,14 @@
-extends AnimatedSprite
-
-var end_point
-
-func _ready():
-	end_point = self.get_node("EndPoint")
-	
-	set_process(true)
-	pass
+extends "res://JetpackGameMode/BG/BG_LooperH.gd"
 
 func _process(delta):
 	#print("Global Pos: %s | Pos: %s"%[self.global_position, self.position])
-	if end_point.global_position.x < 0:
+	if end_point.global_position.x < 0: 
 		#print("Move Sprite!")
-		frame = floor(rand_range(0.0,5.0))
+		if get_owner().current_level == 5:
+			frame = 3
+		else:
+			frame = floor(rand_range(0.0,5.0))
 		var new_pos = Vector2(self.position.x + (2*2020), self.position.y)
 		self.position = new_pos
 	pass
+
--- JetpackGameMode/BG/BG_LooperH_Bg3.gd
+++ JetpackGameMode/BG/BG_LooperH_Bg3.gd
@@ -1,14 +1,14 @@
 extends "res://JetpackGameMode/BG/BG_LooperH.gd"
 
 func _process(delta):
-	#print("Global Pos: %s | Pos: %s"%[self.get_global_position(), self.get_position()])  #-- NOTE: Automatically converted by Godot 2 to 3 converter, please review
-	if end_point.get_global_position().x < 0:  #-- NOTE: Automatically converted by Godot 2 to 3 converter, please review
+	#print("Global Pos: %s | Pos: %s"%[self.global_position, self.position])
+	if end_point.global_position.x < 0: 
 		#print("Move Sprite!")
-		var frame = floor(rand_range(0.0,5.0))
 		if get_owner().current_level == 5:
-			frame = 2
-		self.set_frame(frame)
-		var new_pos = Vector2(self.get_position().x + (2*2020), self.get_position().y)  #-- NOTE: Automatically converted by Godot 2 to 3 converter, please review
-		self.set_position(new_pos)  #-- NOTE: Automatically converted by Godot 2 to 3 converter, please review
+			frame = 5
+		else:
+			frame = floor(rand_range(0.0,self.frames.get_frame_count(self.animation)))
+		var new_pos = Vector2(self.position.x + (2*2020), self.position.y)
+		self.position = new_pos
 	pass
 
