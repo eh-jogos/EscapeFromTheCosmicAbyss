c774b1f6cbfe1bb8b7d485dc048f5c4f77a497b5
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-05-06 14:06:55 -0300

    Fix some keyboard navigations bugs on ColorsMenu

--- CommonScenes/ColorsMenu/NavButtons.gd
+++ CommonScenes/ColorsMenu/NavButtons.gd
@@ -4,7 +4,12 @@ func _ready():
 	Global.connect("page_updated", self, "_on_Global_page_updated")
 
 
-func _on_Global_page_updated(first_button, last_button):
+func _on_Global_page_updated(first_button: Button, last_button: Button) -> void:
+	var exit_button = $Exit
 	for child in get_children():
-		child.set_focus_neighbour(MARGIN_BOTTOM, first_button.get_path())
-		child.set_focus_neighbour(MARGIN_TOP, last_button.get_path())
+		var button: Button = child as Button
+		button.focus_neighbour_bottom = button.get_path_to(first_button)
+		button.focus_neighbour_top = button.get_path_to(last_button)
+	
+	first_button.focus_neighbour_top = first_button.get_path_to(exit_button)
+	last_button.focus_neighbour_bottom = last_button.get_path_to(exit_button)
