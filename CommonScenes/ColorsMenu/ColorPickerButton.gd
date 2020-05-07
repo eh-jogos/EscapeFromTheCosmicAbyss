540a654af2bc6143bd3bd607168a7010135273de
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-05-07 08:47:22 -0300

    Update ColorPicerButton script to 3.2 appeareance and add joypad support
    And keyboard support as well

--- CommonScenes/ColorsMenu/ColorPickerButton.gd
+++ CommonScenes/ColorsMenu/ColorPickerButton.gd
@@ -3,10 +3,6 @@ extends ColorPickerButton
 var category = ""
 var selected_part = ""
 
-# fix for using Global on tool
-var Global = null
-var SoundManager = null
-
 onready var back_panel = self.get_popup()
 onready var color_picker_container = back_panel.get_child(0)
 
@@ -18,11 +14,14 @@ onready var color_preview_line = color_picker_container.get_child(1)
 onready var color_preview = color_preview_line.get_child(0)
 onready var color_pick_button = color_preview_line.get_child(1)
 
+onready var separator1 = color_picker_container.get_child(3)
+onready var separator2 = color_picker_container.get_child(5)
+
 onready var sliders_container = color_picker_container.get_child(4)
 onready var red_slider_line = sliders_container.get_child(0)
 onready var green_slider_line = sliders_container.get_child(1)
 onready var blue_slider_line = sliders_container.get_child(2)
-onready var separator_line = sliders_container.get_child(3)
+onready var slider_separator_line = sliders_container.get_child(3)
 onready var options_line = sliders_container.get_child(4)
 onready var hsv_mode_option = options_line.get_child(0)
 onready var raw_mode_option = options_line.get_child(1)
@@ -41,28 +40,20 @@ func _ready():
 	connect("focus_entered", self, "_on_focus_entered")
 	connect("focus_exited", self, "_on_focus_exited")
 	back_panel.connect("popup_hide", self, "_on_popup_hide")
-	
-	if not Engine.is_editor_hint():
-		Global = get_node("/root/Global")
-		SoundManager = get_node("/root/SoundManager")
-
+	Global.connect("reset_color_pickers", self, "_on_Global_reset_color_pickers")
 
 func set_current_color():
-	if Engine.is_editor_hint():
-		return
-	
-	if Global:
-		color = Global.savedata.colors[category][selected_part]
+	color = Global.savedata.colors[category][selected_part]
 
 
 func _on_color_changed(color):
-	if Engine.is_editor_hint():
-		return
-	
-	if Global:
-		Global.savedata.colors[category][selected_part] = color
+	Global.savedata.colors[category][selected_part] = color
 	
 	get_tree().call_group("interactive_color", "colors_changed")
+
+
+func _on_Global_reset_color_pickers():
+	set_current_color()
 
 
 func _on_popup_hide():
@@ -70,9 +61,7 @@ func _on_popup_hide():
 	$Cooldown.start()
 	yield(get_tree(), "idle_frame")
 	
-	if SoundManager:
-		SoundManager.play_sfx("Change")
-	
+	SoundManager.play_sfx("Change")
 	
 	self.grab_focus()
 
@@ -83,8 +72,7 @@ func _on_focus_entered():
 
 
 func _on_focus_exited():
-	if SoundManager:
-		SoundManager.play_sfx("Select")
+	SoundManager.play_sfx("Select")
 	var description = get_node("Description")
 	description.add_color_override("font_color", Color("00f5ff"))
 
@@ -94,25 +82,78 @@ func _on_ButtonArea_mouse_enter():
 
 
 func _on_ColorPickerButton_gui_input( event ):
-	if not Global:
-		return
-	
 	if event.is_action_pressed("ui_right"):
 		Global.emit_signal("navigated_to_right")
 	elif event.is_action_pressed("ui_left"):
 		Global.emit_signal("navigated_to_left")
 
 
+func _process(_delta):
+	if not back_panel.visible:
+		return
+	
+	var has_color_changed = false
+	
+	has_color_changed = _handle_color_input("hue")
+	has_color_changed = _handle_color_input("saturation")
+	has_color_changed = _handle_color_input("value")
+	
+	if has_color_changed:
+		emit_signal("color_changed", color)
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		back_panel.hide()
+
+
+func _handle_color_input(hsv_option: String) -> bool:
+	var has_color_changed = false
+	var action_up: String = ""
+	var action_down: String = ""
+	var color_string: String = ""
+	match hsv_option:
+		"hue":
+			action_up = "color_hue_down"
+			action_down = "color_hue_up"
+			color_string = "h"
+		"saturation":
+			action_up = "color_saturation_up"
+			action_down = "color_saturation_down"
+			color_string = "s"
+		"value":
+			action_up = "color_value_up"
+			action_down = "color_value_down"
+			color_string = "v"
+		_:
+			push_warning("Unindentified option: %s | Must be one of"
+					+ "hue, saturation or value"%[hsv_option])
+			return false
+	
+	has_color_changed = true
+	
+	if Input.is_action_pressed(action_up):
+		var strength = Input.get_action_strength(action_up)
+		color[color_string] = clamp(color[color_string] + 0.01 * abs(strength), 0.0, 1.0)
+		print("color.%s changed: %s | strength: %s"%[color_string, color[color_string], strength])
+	elif Input.is_action_pressed(action_down):
+		var strength = Input.get_action_strength(action_down)
+		color[color_string] = clamp(color[color_string] - 0.01 * abs(strength), 0.0, 1.0)
+		print("color.%s changed: %s | strength: %s"%[color_string, color[color_string], strength])
+	
+	
+	return has_color_changed
+
+
+func _on_Cooldown_timeout():
+	#print("Active Again")
+	_button_area.mouse_filter = Control.MOUSE_FILTER_PASS
+
+
 func _set_color_picker_appearence() -> void:
 	color_preview_line.hide()
-	red_slider_line.hide()
-	green_slider_line.hide()
-	blue_slider_line.hide()
-	separator_line.hide()
-	hsv_mode_option.hide()
-	raw_mode_option.hide()
-	toggle_output_button.hide()
+	sliders_container.hide()
 	presets_line.hide()
+	separator1.hide()
+	separator2.hide()
 	
 	hue_slider.set_custom_minimum_size(Vector2(40,0))
 	back_panel.rect_min_size = Vector2.ONE * 400
@@ -132,15 +173,9 @@ func _name_color_picker_elements() -> void:
 	red_slider_line.name = "RedSliderLine"
 	green_slider_line.name = "GreenSliderLine"
 	blue_slider_line.name = "BlueSliderLine"
-	separator_line.name = "SeparatorLine"
+	slider_separator_line.name = "SeparatorLine"
 	hsv_mode_option.name = "HSVModeOption"
 	raw_mode_option.name = "RawModeOption"
 	toggle_output_button.name = "ToggleOutputFormatButton"
 	hex_code_line_edit.name = "HexCodeLineEdit"
 	presets_line.name = "PresetsLine"
-
-
-func _on_Cooldown_timeout():
-	print("Active Again")
-	_button_area.mouse_filter = Control.MOUSE_FILTER_PASS
-	
