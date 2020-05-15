bcdcc8b95a5f5aff46654f4904fddff33c56dedb
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-05-15 15:25:02 -0300

    Add script to automate generating builds

---
+++ build_releases.sh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+export_configs="export_presets.cfg"
+godot_version="32"
+base_builds_path="../GameBuilds/"
+project_name="CosmicAbyss"
+version=$1
+debug=$2
+
+fpath=( ~/.zfunc "${fpath[@]}" )
+autoload -Uz godot
+
+function getProperty {
+   prop_key=$1
+   prop_value=$(cat $export_configs | grep $prop_key | cut -d'=' -f2)
+   echo $prop_value
+}
+
+export_profiles=(${=$(getProperty "^name")})
+echo "Going to Export the following profiles: $export_profiles"
+export_paths=(${=$(getProperty "^export_path")})
+length=${#export_profiles[@]}
+for ((i = 1; i <= $length; i++));
+do
+	profile=${export_profiles[i]}
+	unquoted_profile=$(sed -e 's/^"//' -e 's/"$//' <<< $profile)
+	unquoted_filename=$(sed -e 's/^"//' -e 's/"$//' <<< ${export_paths[i]})
+	
+	final_path_release=$base_builds_path$version"/Release/"$project_name$unquoted_profile"/"
+	mkdir -p $final_path_release
+	godot godot_version --export $unquoted_profile $final_path_debug$unquoted_filename
+	
+	final_path_debug=$base_builds_path$version"/Debug/"$project_name$unquoted_profile"/"
+	mkdir -p $final_path_debug
+	godot godot_version --export-debug $unquoted_profile $final_path_release$unquoted_filename
+done
