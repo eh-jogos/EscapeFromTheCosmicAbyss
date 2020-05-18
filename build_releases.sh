1cccfef8a7b855605ebf199d0a82844670bbc56e
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-05-18 10:45:47 -0300

    fix some bugs in build_releases script

--- build_releases.sh
+++ build_releases.sh
@@ -21,15 +21,21 @@ export_paths=(${=$(getProperty "^export_path")})
 length=${#export_profiles[@]}
 for ((i = 1; i <= $length; i++));
 do
+	echo "###########################################################"
 	profile=${export_profiles[i]}
 	unquoted_profile=$(sed -e 's/^"//' -e 's/"$//' <<< $profile)
 	unquoted_filename=$(sed -e 's/^"//' -e 's/"$//' <<< ${export_paths[i]})
-	
 	final_path_release=$base_builds_path$version"/Release/"$project_name$unquoted_profile"/"
-	mkdir -p $final_path_release
-	godot godot_version --export $unquoted_profile $final_path_debug$unquoted_filename
-	
 	final_path_debug=$base_builds_path$version"/Debug/"$project_name$unquoted_profile"/"
+	
+	echo "Exporting $unquoted_profile Release to $final_path_release"
+	mkdir -p $final_path_release
+	godot godot_version --export $unquoted_profile $final_path_release$unquoted_filename
+	
+	echo "Exporting $unquoted_profile Debug to $final_path_debug"
 	mkdir -p $final_path_debug
-	godot godot_version --export-debug $unquoted_profile $final_path_release$unquoted_filename
+	godot godot_version --export-debug $unquoted_profile $final_path_debug$unquoted_filename
+
+	echo "Exporting $unquoted_profile Finished"
+	echo "###########################################################"
 done
