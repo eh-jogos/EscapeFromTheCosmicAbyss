4ba6253349fafc7235997213a1d3ca053889c8a6
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-05-22 16:15:01 -0300

    update build_releases script to be more extensible

--- build_releases.sh
+++ build_releases.sh
@@ -1,13 +1,6 @@
 #!/bin/zsh
 export_configs="export_presets.cfg"
-godot_version="32"
-base_builds_path="../GameBuilds/"
-project_name="CosmicAbyss"
 version=$1
-debug=$2
-
-fpath=( ~/.zfunc "${fpath[@]}" )
-autoload -Uz godot
 
 function getProperty {
    prop_key=$1
@@ -18,24 +11,17 @@ function getProperty {
 export_profiles=(${=$(getProperty "^name")})
 echo "Going to Export the following profiles: $export_profiles"
 export_paths=(${=$(getProperty "^export_path")})
+
 length=${#export_profiles[@]}
 for ((i = 1; i <= $length; i++));
 do
-	echo "###########################################################"
 	profile=${export_profiles[i]}
-	unquoted_profile=$(sed -e 's/^"//' -e 's/"$//' <<< $profile)
-	unquoted_filename=$(sed -e 's/^"//' -e 's/"$//' <<< ${export_paths[i]})
-	final_path_release=$base_builds_path$version"/Release/"$project_name$unquoted_profile"/"
-	final_path_debug=$base_builds_path$version"/Debug/"$project_name$unquoted_profile"/"
-	
-	echo "Exporting $unquoted_profile Release to $final_path_release"
-	mkdir -p $final_path_release
-	godot godot_version --export $unquoted_profile $final_path_release$unquoted_filename
-	
-	echo "Exporting $unquoted_profile Debug to $final_path_debug"
-	mkdir -p $final_path_debug
-	godot godot_version --export-debug $unquoted_profile $final_path_debug$unquoted_filename
-
-	echo "Exporting $unquoted_profile Finished"
-	echo "###########################################################"
+	# This is to accept other modifiers in case you want
+	# to have separate scripts for building steam releases
+	# or building itch.io releases. Just add them as cases below
+	case $2 in
+		*)
+			./build_standalone_releases.sh $version $profile
+			;;
+	esac
 done
---
+++ build_standalone_releases.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+version=$1
+profile=$2
+
+echo
+echo "Building Standalone release for: $profile"
+echo "Game Version is: $version"
+
+godot_version="32"
+base_builds_path="../GameBuilds/"
+project_name="CosmicAbyss"
+
+fpath=( ~/.zfunc "${fpath[@]}" )
+autoload -Uz godot
+
+echo "###########################################################"
+# unquoted_profile=$(sed -e 's/^"//' -e 's/"$//' <<< $profile)
+# unquoted_filename=$(sed -e 's/^"//' -e 's/"$//' <<< $profile)
+# final_path_release=$base_builds_path$version"/Release/"$project_name$unquoted_profile"/"
+# final_path_debug=$base_builds_path$version"/Debug/"$project_name$unquoted_profile"/"
+
+echo "Exporting $unquoted_profile Release to $final_path_release"
+# mkdir -p $final_path_release
+# godot godot_version --export $unquoted_profile $final_path_release$unquoted_filename
+
+echo "Exporting $unquoted_profile Debug to $final_path_debug"
+# mkdir -p $final_path_debug
+# godot godot_version --export-debug $unquoted_profile $final_path_debug$unquoted_filename
+
+echo "Exporting $unquoted_profile Finished"
+echo "###########################################################"
+echo
