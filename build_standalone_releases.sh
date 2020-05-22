63ec26a941d34cfadc26bc3ff091f2c6e1e859a5
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-05-22 16:25:13 -0300

    uncomment lines I had forgotten do uncomment in build_standalone_releases

--- build_standalone_releases.sh
+++ build_standalone_releases.sh
@@ -14,18 +14,18 @@ fpath=( ~/.zfunc "${fpath[@]}" )
 autoload -Uz godot
 
 echo "###########################################################"
-# unquoted_profile=$(sed -e 's/^"//' -e 's/"$//' <<< $profile)
-# unquoted_filename=$(sed -e 's/^"//' -e 's/"$//' <<< $profile)
-# final_path_release=$base_builds_path$version"/Release/"$project_name$unquoted_profile"/"
-# final_path_debug=$base_builds_path$version"/Debug/"$project_name$unquoted_profile"/"
+unquoted_profile=$(sed -e 's/^"//' -e 's/"$//' <<< $profile)
+unquoted_filename=$(sed -e 's/^"//' -e 's/"$//' <<< $profile)
+final_path_release=$base_builds_path$version"/Release/"$project_name$unquoted_profile"/"
+final_path_debug=$base_builds_path$version"/Debug/"$project_name$unquoted_profile"/"
 
 echo "Exporting $unquoted_profile Release to $final_path_release"
-# mkdir -p $final_path_release
-# godot godot_version --export $unquoted_profile $final_path_release$unquoted_filename
+mkdir -p $final_path_release
+godot godot_version --export $unquoted_profile $final_path_release$unquoted_filename
 
 echo "Exporting $unquoted_profile Debug to $final_path_debug"
-# mkdir -p $final_path_debug
-# godot godot_version --export-debug $unquoted_profile $final_path_debug$unquoted_filename
+mkdir -p $final_path_debug
+godot godot_version --export-debug $unquoted_profile $final_path_debug$unquoted_filename
 
 echo "Exporting $unquoted_profile Finished"
 echo "###########################################################"
