8022160ac99b4b12c09bf17a623ba0b1795d7c58
Daniel Queiroz Porto <danielvandalk@gmail.com>
2020-06-16 07:57:48 -0300

    fix build script not using the godot_version variable correctly

--- build_standalone_releases.sh
+++ build_standalone_releases.sh
@@ -23,13 +23,13 @@ final_path_debug=$base_builds_path/$version/Debug/$project_name$unquoted_profile
 
 echo "Exporting $unquoted_profile Release to $final_path_release"
 mkdir -p $final_path_release
-godot godot_version --export $unquoted_profile $final_path_release$filename
+godot $godot_version --export $unquoted_profile $final_path_release$filename
 
 if [[ $include_debug = "true" ]]
 then
 	echo "Exporting $unquoted_profile Debug to $final_path_debug"
 	mkdir -p $final_path_debug
-	godot godot_version --export-debug $unquoted_profile $final_path_debug$filename
+	godot $godot_version --export-debug $unquoted_profile $final_path_debug$filename
 fi
 
 echo "Exporting $unquoted_profile Finished"
